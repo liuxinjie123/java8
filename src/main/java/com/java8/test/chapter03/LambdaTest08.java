58c99a06d572e858d4bd3b013643769fd09c2f26
liuxinjie123 <jackcodejs@gmail.com>
2017-10-24 22:59:39 +0800

    chapter03

---
+++ src/main/java/com/java8/test/chapter03/LambdaTest08.java
@@ -0,0 +1,19 @@
+package com.java8.test.chapter03;
+
+import com.java8.test.chapter01.FilterApplesTest;
+
+import java.util.function.Predicate;
+
+public class LambdaTest08 {
+    public interface IntPredicate {
+        boolean test(int t);
+    }
+
+
+    public static void main(String[] args) {
+        IntPredicate evenNumbers = (int i) -> i % 2 == 0;
+        evenNumbers.test(1000);
+        Predicate<Integer> oddNumbers = (Integer i) -> i % 2 == 1;
+        oddNumbers.test(1000);
+    }
+}
