c5c385e3b5ed629c25ee384d7faeb8be283b257e
liuxinjie123 <jackcodejs@gmail.com>
2017-11-12 14:28:52 +0800

    chapter05

---
+++ src/main/java/com/java8/test/chapter05/StreamTest03.java
@@ -0,0 +1,37 @@
+package com.java8.test.chapter05;
+
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.stream.Stream;
+
+public class StreamTest03 {
+    public static void main(String[] args) {
+        System.out.println(" ----------------- 001 ---------------- ");
+        Stream<String> stream = Stream.of("Java8", "lambda", "Action");
+        stream.map(String::toUpperCase).forEach(System.out::println);
+
+        System.out.println(" ----------------- 002 ---------------- ");
+
+        int[] numbers = {1, 2, 3, 4, 5};
+        int intSum = Arrays.stream(numbers).sum();
+        System.out.println("intSum=" + intSum);
+
+        System.out.println(" ----------------- 003 ---------------- ");
+
+        long uniqueWords = 0;
+        try (
+            Stream<String> lines = Files.lines(Paths.get("files/chapter05/data.txt"), Charset.defaultCharset())
+        ) {
+            uniqueWords = lines.flatMap(line -> Arrays.stream(line.split(" ")))
+                    .distinct()
+                    .count();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        System.out.println(" uniqueWords=" + uniqueWords);
+
+    }
+}
