069d1d7915069dd4b588efddb6ec665b62a054a8
liuxinjie <jackdream@163.com>
2021-04-05 18:16:53 +0800

    test foreach foreachOrdered

--- src/test/java/com/java8/ApplicationRunTest.java
+++ src/test/java/com/java8/ApplicationRunTest.java
@@ -1,16 +1,20 @@
 package com.java8;
 
 import lombok.extern.slf4j.Slf4j;
-import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 import static java.util.Arrays.asList;
 import static java.util.stream.Collectors.toList;
+import static org.hamcrest.Matchers.hasItem;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThat;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -20,8 +24,62 @@ public class ApplicationRunTest {
     public void test001() {
         List<Integer> numbers = asList(1, 3, 66, 6, 9);
         List<Integer> sameOrder = numbers.stream().collect(toList());
-        Assert.assertEquals(numbers, sameOrder);
-        Assert.assertEquals(asList(9, 1, 3, 66, 6), sameOrder);
+        assertEquals(numbers, sameOrder);
+        assertEquals(asList(9, 1, 3, 66, 6), sameOrder);
+    }
+
+    @Test
+    public void testSort() {
+        Set<Integer> numbers = new HashSet<>(asList(4, 3, 1, 2));
+        List<Integer> sameOrder = numbers.stream()
+                .sorted()
+                .collect(toList());
+        assertEquals(asList(1, 2, 3, 4), sameOrder);
+    }
+
+    @Test
+    public void testUnordered() {
+        List<Integer> numbers = asList(1, 2, 3, 4);
+        List<Integer> stillOrdered = numbers.stream()
+                .map(x -> x+1)
+                .collect(toList());
+        // sorted
+        assertEquals(asList(2, 3, 4, 5), stillOrdered);
+
+        Set<Integer> unOrdered = new HashSet<>(numbers);
+        List<Integer> stillUnOrdered = unOrdered.stream()
+                .map(x -> x+1)
+                .collect(toList());
+        // 顺序得不到保证
+        assertThat(stillUnOrdered, hasItem(2));
+        assertThat(stillUnOrdered, hasItem(3));
+        assertThat(stillUnOrdered, hasItem(4));
+        assertThat(stillUnOrdered, hasItem(5));
+//        assertThat(stillUnOrdered, hasItem(1));
+    }
+
+    @Test
+    public void testForeach() {
+        List<Integer> numbers = asList(1, 2, 3, 4);
+        numbers.stream()
+                .forEach(System.out::print);
+
+        System.out.println();
+
+        numbers.stream()
+                .forEachOrdered(System.out::print);
+
+        System.out.println();
+
+        numbers.parallelStream()
+                .forEach(System.out::print);
+
+        System.out.println();
+
+        numbers.parallelStream()
+                .forEachOrdered(System.out::print);
+
+        System.out.println();
     }
 
 }
